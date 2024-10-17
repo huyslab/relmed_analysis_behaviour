259b4bd67989e91622aa4c11a6ec7f68510c197d
yanivabir <yanivabir@gmail.com>
2024-10-17 22:11:28 +0100

    work on model free set size effects

--- data_analysis/pilot2_PILT.jl
+++ data_analysis/pilot2_PILT.jl
@@ -12,11 +12,12 @@ begin
     Pkg.activate("relmed_environment")
     # instantiate, i.e. make sure that all packages are downloaded
     Pkg.instantiate()
-	using Random, DataFrames, JSON, CSV, StatsBase, JLD2, HTTP, CairoMakie, Printf, Distributions, CategoricalArrays, AlgebraOfGraphics, Dates
+	using Random, DataFrames, JSON, CSV, StatsBase, JLD2, HTTP, CairoMakie, Printf, Distributions, CategoricalArrays, AlgebraOfGraphics, Dates, Turing
 	using LogExpFunctions: logistic, logit
 	include("fetch_preprocess_data.jl")
 	include("sample_utils.jl")
 	include("plotting_utils.jl")
+	include("stats_utils.jl")
 	nothing
 end
 
@@ -47,7 +48,7 @@ end
 # Load data
 begin
 	PLT_data, test_data, vigour_data, jspsych_data = load_pilot2_data()
-	PLT_data = exclude_PLT_sessions(PLT_data, required_n_blocks = 0)
+	PLT_data = exclude_PLT_sessions(PLT_data, required_n_blocks = 18)
 end
 
 # ╔═╡ 21b3018f-4856-4b34-b7d2-f3549a6fc556
@@ -80,7 +81,7 @@ let
 	# Split blocks into two halves
 	blocks = unique(set_size_diff_split[!, [:block, :n_pairs]])
 	
-	transform!(
+	DataFrames.transform!(
 		groupby(blocks, :n_pairs), 
 		:block => (x -> 1:length(x)) => :reblock) 
 
@@ -126,13 +127,21 @@ let
 
 	# Remove missing due to missing data
 	dropmissing!(set_size_diff_split)
+
+	r = spearman_brown(cor(set_size_diff_split.split_1, set_size_diff_split.split_2))
 	
-	mp2 = data(set_size_diff_split) * mapping(
+	mp2 = 
+		data(set_size_diff_split) * mapping(
 		:split_1 => "Set size 3 - 1 odd blocks",
 		:split_2 => "Set size 3 - 1 even blocks"
-	) * (linear() + visual(Scatter))
+	) * (linear() + visual(Scatter)) +
+		mapping([0], [1]) * visual(ABLines, color = :grey, linestyle = :dash)
 
-	draw!(f[1,2], mp2)
+	draw!(f[1,2], mp2, 
+		axis = (; subtitle = "Spearman-Brown r=$(round(r, digits = 2))")
+	)
+
+	save("results/pilot2_acc_set_size.png", f, pt_per_unit = 1)
 
 	f
 end
@@ -146,10 +155,14 @@ let
 	ax = plot_group_accuracy!(
 		f[1,1],
 		group = :n_pairs,
-		PLT_data
+		PLT_data,
+		legend = Dict(i => "$i" for i in 1:3),
+		legend_title = "Set size"
 	)
 
 	ax.xticks = [1, 10, 20, 30]
+
+	save("results/pilot2_acc_curve.png", f, pt_per_unit = 1)
 
 	f
 
@@ -161,7 +174,7 @@ let
 
 	PLT_remmaped = copy(PLT_data)
 
-	transform!(
+	DataFrames.transform!(
 		groupby(PLT_remmaped, [:session, :prolific_pid, :exp_start_time, :block, :stimulus_pair]),
 		:trial => (x -> 1:length(x)) => :trial
 	)
@@ -171,14 +184,81 @@ let
 	ax = plot_group_accuracy!(
 		f[1,1],
 		group = :n_pairs,
-		PLT_remmaped
+		PLT_remmaped,
+		legend = Dict(i => "$i" for i in 1:3),
+		legend_title = "Set size"
 	)
 
 	ax.xticks = [1, 10, 20, 30]
 
+	save("results/pilot2_acc_curve_realigned.png", f, pt_per_unit = 1)
+
 	f
 
 end
+
+# ╔═╡ 22d12a3f-c8ca-4c46-8ff3-b7e1432a77d5
+# Plot accuracy by switch / repeat
+let
+	f = Figure(
+		#size = (700, 300)
+	)
+	
+	DataFrames.transform!(
+		groupby(PLT_data, [:prolific_pid, :block]),
+		:stimulus_pair => (x -> [missing; x[1:end-1]]) => :prev_pair
+	)
+
+	switch_acc = filter(x -> !ismissing(x.prev_pair), PLT_data)
+	
+	switch_acc.switch = ifelse.(
+		switch_acc.stimulus_pair .== switch_acc.prev_pair,
+		fill("no", nrow(switch_acc)),
+		fill("yes", nrow(switch_acc))
+	)
+
+	switch_acc_sum = combine(
+		groupby(switch_acc, 
+			[:prolific_pid, :switch]),
+		:isOptimal => mean => :acc
+	)
+
+
+	mp1 = data(switch_acc_sum) * mapping(:switch => "Switch trial", :acc => "Proportion optimal choice", color = :switch) * visual(RainClouds)
+	
+	draw!(f[1,1], mp1)
+
+	switch_acc.split = ifelse.(
+		switch_acc.block .> 9, 
+		fill(2, nrow(switch_acc)),
+		fill(1, nrow(switch_acc))
+	)
+
+	switch_acc_split = combine(
+		groupby(switch_acc, [:prolific_pid, :split, :switch]),
+		:isOptimal => mean => :acc
+	)
+
+	# Long to wide
+	switch_acc_split = unstack(
+		switch_acc_split,
+		[:prolific_pid, :split],
+		:switch,
+		:isOptimal
+	)
+
+	# r = spearman_brown(cor(switch_acc.yes, switch_acc.no)) 
+
+	# mp2 = data(switch_acc) * mapping(:yes, :no) * (linear() + visual(Scatter)) +
+	# 	mapping([0], [1]) * visual(ABLines, color = :grey, linestyle = :dash)
+
+	# draw!(f[1,2], mp2; axis = (; subtitle = "Spearman-Brown r=$(round(r, digits = 2))"))
+
+	# fs
+end
+
+# ╔═╡ 89d32f15-a670-449c-a17a-7dc37ca503b9
+describe(PLT_data)
 
 # ╔═╡ Cell order:
 # ╠═ff8c6dd6-7b4a-11ef-3148-ad43f7d07801
@@ -188,3 +268,5 @@ end
 # ╠═ec053072-8386-4000-b425-88496b82bb1d
 # ╠═60559c55-840f-49cb-9537-1aceaaf5e658
 # ╠═cb470684-4557-4cb4-a6c2-90a207d4dcc6
+# ╠═22d12a3f-c8ca-4c46-8ff3-b7e1432a77d5
+# ╠═89d32f15-a670-449c-a17a-7dc37ca503b9
