0146aadc23b9a6af06d75ce0acabca4b42dd4559
yanivabir <yanivabir@gmail.com>
2024-10-28 12:45:09 +0000

    add load pilot 4x to fetch

--- fetch_preprocess_data.jl
+++ fetch_preprocess_data.jl
@@ -134,6 +134,44 @@ function prepare_PLT_data(data::DataFrame)
 
 end
 
+function load_pilot4x_data(; force_download = false)
+	datafile = "data/pilot4.x.jld2"
+
+	# Load data or download from REDCap
+	if !isfile(datafile) || force_download
+		jspsych_json, records = get_REDCap_data("pilot4.x"; file_field = "file_data")
+	
+		jspsych_data = REDCap_data_to_df(jspsych_json, records)
+
+		remove_testing!(jspsych_data)
+
+		JLD2.@save datafile jspsych_data
+	else
+		JLD2.@load datafile jspsych_data
+	end
+
+	# Exctract PILT
+	PLT_data = prepare_PLT_data(jspsych_data)
+
+	# Extract post-PILT test
+	test_data = prepare_post_PILT_test_data(jspsych_data)
+
+	# Exctract vigour
+	vigour_data = prepare_vigour_data(jspsych_data) 
+
+	# Extract post-vigour test
+	post_vigour_test_data = prepare_post_vigour_test_data(jspsych_data)
+
+	# Extract PIT
+	PIT_data = prepare_PIT_data(jspsych_data)
+
+	# Exctract reversal
+	reversal_data = prepare_reversal_data(jspsych_data)
+
+	return PLT_data, test_data, vigour_data, post_vigour_test_data, PIT_data, reversal_data, jspsych_data
+end
+
+
 function load_pilot4_data()
 	datafile = "data/pilot4.jld2"
 
