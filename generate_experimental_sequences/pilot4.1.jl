8b0655b293b2a386e358fa731c8d67c4514c48a1
yanivabir <yanivabir@gmail.com>
2024-10-28 12:39:39 +0000

    post-pilt test sequence

--- generate_experimental_sequences/pilot4.1.jl
+++ generate_experimental_sequences/pilot4.1.jl
@@ -82,7 +82,7 @@ end
 
 # ╔═╡ a57e46e9-7a8f-4190-90d6-62272c8fb036
 # Assign valence and set size per block
-valence_set_size = let random_seed = 1
+valence_set_size = let random_seed = 2
 	
 	# # All combinations of set sizes and valence
 	@assert iseven(block_per_set) # Requisite for code below
@@ -307,7 +307,258 @@ end
 # ╔═╡ ccfb8336-4ea0-4474-99b9-cdbf02d8fd42
 let
 	save_to_JSON(task, "results/pilot4.1_pilt.json")
-	CSV.write("results/pilot4_pilt.1.csv", task)
+	CSV.write("results/pilot4.1_pilt.csv", task)
+end
+
+# ╔═╡ f283d6d9-6d06-44e8-b6bc-63d54ad28b4a
+md"""# Post-PILT test"""
+
+# ╔═╡ 55263a95-883e-4ec4-9967-1a9002422c67
+function create_test_sequence(
+	pilt_task::DataFrame;
+	random_seed::Int64, 
+	same_weight::Float64 = 6.5
+) 
+	
+	rng = Xoshiro(random_seed)
+
+	# Extract stimuli and their common feedback from task structure
+	stimuli = vcat([rename(
+		pilt_task[pilt_task.feedback_common, [:session, :n_pairs, :block, :cpair, Symbol("stimulus_$s"), Symbol("feedback_$s")]],
+		Symbol("stimulus_$s") => :stimulus,
+		Symbol("feedback_$s") => :feedback
+	) for s in ["right", "left"]]...)
+
+	# Summarize magnitude per stimulus
+	stimuli = combine(
+		groupby(stimuli, [:session, :n_pairs, :block, :cpair, :stimulus]),
+		:feedback => (x -> mean(unique(x))) => :magnitude
+	)
+
+	# Step 1: Identify unique stimuli
+	unique_stimuli = unique(stimuli.stimulus)
+
+	# Step 2: Define existing pairs
+	create_pair_list(d) = [filter(x -> x.cpair == p, d).stimulus 
+		for p in unique(stimuli.cpair)]
+
+	existing_pairs = create_pair_list(stimuli)
+
+	# Step 3: Generate all possible pairs
+	all_possible_pairs = unique(sort.(collect(combinations(unique_stimuli, 2))))
+
+	# Step 6: Select pairs ensuring each stimulus is used once and magnitudes are balanced
+	final_pairs = []
+	used_stimuli = Set{String}()
+
+	# Create a priority queue for balanced selection based on pair counts
+	pair_counts = Dict{Vector{Float64}, Int}()
+
+	# Function to retrieve attribute of stimulus
+	stim_attr(s, attr) = stimuli[stimuli.stimulus .== s, :][!, attr][1]
+
+	for b in 1:test_n_blocks
+
+		# Step 4: Filter valid pairs: were not paired in PILT, ano same category
+		valid_pairs = 
+			filter(pair -> 
+				!(pair in existing_pairs) && 
+				!(reverse(pair) in existing_pairs) && 
+				(pair[1][1:(end-5)] != pair[2][1:(end-5)]), 
+			all_possible_pairs)
+	
+		# Step 5: Create a mapping of pairs to their magnitudes
+		magnitude_pairs = Dict{Vector{Float64}, Vector{Vector{String}}}()
+		
+		for pair in valid_pairs
+		    mag1 = stimuli[stimuli.stimulus .== pair[1], :].magnitude[1]
+		    mag2 = stimuli[stimuli.stimulus .== pair[2], :].magnitude[1]
+		    key = sort([mag1, mag2])
+		    if !haskey(magnitude_pairs, key)
+		        magnitude_pairs[key] = []
+		    end
+		    push!(magnitude_pairs[key], pair)
+		end
+	
+		@assert sum(length(vec) for vec in values(magnitude_pairs)) == length(valid_pairs)
+	
+		# Step 5.5 - Shuffle order within each magnitude
+		for (k, v) in magnitude_pairs
+			magnitude_pairs[k] = shuffle(rng, v)
+		end
+
+		# Initialize counts
+		if b == 1
+			for key in keys(magnitude_pairs)
+			    pair_counts[key] = 0
+			end
+		end
+		
+		block_pairs = []
+		
+		while true
+		    found_pair = false
+	
+		    # Select pairs while balancing magnitudes
+		    for key in sort(collect(keys(magnitude_pairs)), by = x -> pair_counts[x] + same_weight * (x[1] == x[2])) # Sort by count, putting equal magnitude las
+		        pairs = magnitude_pairs[key]
+	
+				# First try to find a same block pair
+		        for pair in pairs
+		            if !(pair[1] in used_stimuli) && !(pair[2] in used_stimuli)  && 
+						stim_attr(pair[1], "block") == stim_attr(pair[2], "block")
+					
+		                push!(block_pairs, pair)
+		                push!(used_stimuli, pair[1])
+		                push!(used_stimuli, pair[2])
+		                pair_counts[key] += 1
+		                found_pair = true
+		                break  # Stop going over pairs
+		            end
+		        end
+	
+				# Then try different block pair
+		        for pair in pairs
+		            if !found_pair &&!(pair[1] in used_stimuli) && 
+						!(pair[2] in used_stimuli) 
+					
+		                push!(block_pairs, pair)
+		                push!(used_stimuli, pair[1])
+		                push!(used_stimuli, pair[2])
+		                pair_counts[key] += 1
+		                found_pair = true
+		                break  # Stop going over pairs
+		            end
+		        end
+		        
+		        if found_pair
+		            break  # Restart the outer loop if a pair was found
+		        end
+		    end
+
+			# Alert bad seed
+			if !found_pair
+				return DataFrame(), NaN, NaN, NaN
+			end
+		
+		    if length(used_stimuli) == length(unique_stimuli)
+		        break  # Exit if all stimuli are used or no valid pairs remain
+		    end
+		end
+
+		# Step 7 - Shuffle pair order
+		shuffle!(rng, block_pairs)
+
+		# Add block pairs to final pairs
+		append!(final_pairs, block_pairs)
+
+		# Add block pairs to existing pairs
+		append!(existing_pairs, block_pairs)
+
+		# Empty used_stimuli
+		used_stimuli = []
+	end
+
+	# Shuffle order within each pair
+	shuffle!.(rng, final_pairs)
+
+	# Step 8 - Form DataFrame
+	pairs_df = DataFrame(
+		block = repeat(1:test_n_blocks, inner = length(unique_stimuli) ÷ 2),
+		trial = repeat(1:(length(unique_stimuli) ÷ 2) * test_n_blocks),
+		stimulus_right = [p[2] for p in final_pairs],
+		stimulus_left = [p[1] for p in final_pairs],
+		magnitude_right = [stimuli[stimuli.stimulus .== p[2], :].magnitude[1] for p in final_pairs],
+		magnitude_left = [stimuli[stimuli.stimulus .== p[1], :].magnitude[1] for p in final_pairs],
+		original_block_right = [stimuli[stimuli.stimulus .== p[2], :].block[1] for p in final_pairs],
+		original_block_left = [stimuli[stimuli.stimulus .== p[1], :].block[1] for p in final_pairs]
+	)
+
+	# Same / different block variable
+	pairs_df.same_block = pairs_df.original_block_right .== pairs_df.original_block_left
+
+	# Valence variables
+	pairs_df.valence_left = sign.(pairs_df.magnitude_left)
+	pairs_df.valence_right = sign.(pairs_df.magnitude_right)
+	pairs_df.same_valence = pairs_df.valence_left .== pairs_df.valence_right
+
+	# Compute sequence stats
+	prop_same_block = (mean(pairs_df.same_block)) 
+	prop_same_valence = (mean(pairs_df.same_valence))
+	n_same_magnitude = sum(pairs_df.magnitude_right .== pairs_df.magnitude_left)
+	
+	pairs_df, prop_same_block, prop_same_valence, n_same_magnitude
+end
+
+# ╔═╡ 4a66d706-c33e-4972-b58f-cf00fc3ca283
+# Choose test sequence with best stats
+function find_best_test_sequence(
+	task::DataFrame; # PILT task structure
+	n_seeds::Int64 = 100, # Number of random seeds to try
+	same_weight::Float64 = 4.1 # Weight reducing the number of same magntiude pairs
+) 
+
+	# Initialize stats variables
+	prop_block = []
+	prop_valence = []
+	n_magnitude = []
+
+	# Run over seeds
+	for s in 1:n_seeds
+		_, pb, pv, nm = create_test_sequence(task, random_seed = s, same_weight = same_weight)
+
+		push!(prop_block, pb)
+		push!(prop_valence, pv)
+		push!(n_magnitude, nm)
+	end
+
+	# First, choose a sequence with the minimal number of same-magnitude pairs
+	pass_magnitude = (1:n_seeds)[n_magnitude .== 
+		minimum(filter(x -> !isnan(x), n_magnitude))]
+
+	@assert !isempty(pass_magnitude)
+
+	# Apply magnitude selection
+	prop_block = prop_block[pass_magnitude]
+	prop_valence = prop_valence[pass_magnitude]
+
+	# Compute deviation from goal
+	dev_block = abs.(prop_block .- 1/3)
+	dev_valence = abs.(prop_block .- 0.5)
+
+	# Choose best sequence
+	chosen = pass_magnitude[argmin(dev_block .+ dev_valence)]
+
+	# Return sequence and stats
+	return create_test_sequence(task, random_seed = chosen, same_weight = same_weight)
+end
+
+# ╔═╡ 14c47e11-2735-4e81-9eb0-152a09cbc5ee
+test = let
+	# Find test sequence for main part
+	test, pb, pv, nm = find_best_test_sequence(
+		task,
+		n_seeds = 50, # Number of random seeds to try
+		same_weight = 22. # Weight reducing the number of same magntiude pairs
+	) 
+
+	@info "Proportion of same block pairs: $pb"
+	@info "Proportion of same valence pairs: $pv"
+	@info "Number of same magnitude pairs: $nm"
+
+	# Create magnitude_pair variable
+	test.magnitude_pair = [sort([r.magnitude_left, r.magnitude_right]) for r in eachrow(test)]
+
+	# Create column session
+	test[!, :session] .= 1
+
+	test
+end
+
+# ╔═╡ 385192d4-01d5-40b3-b25c-88267f5f0f3a
+let
+	save_to_JSON(test, "results/pilot4.1_pilt_test.json")
+	CSV.write("results/pilot4.1_pilt_test.csv", test)
 end
 
 # ╔═╡ Cell order:
@@ -320,3 +571,8 @@ end
 # ╠═8dfd1290-7988-4c61-8342-f4accf876bb6
 # ╠═08639317-881c-4af9-bad5-9fdfc98519ed
 # ╠═ccfb8336-4ea0-4474-99b9-cdbf02d8fd42
+# ╟─f283d6d9-6d06-44e8-b6bc-63d54ad28b4a
+# ╠═14c47e11-2735-4e81-9eb0-152a09cbc5ee
+# ╠═385192d4-01d5-40b3-b25c-88267f5f0f3a
+# ╠═55263a95-883e-4ec4-9967-1a9002422c67
+# ╠═4a66d706-c33e-4972-b58f-cf00fc3ca283
