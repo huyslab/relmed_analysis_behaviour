370c530d72f267814433ce7ca5c3ee6e1c160c3e
Haoyang Lu <haoyang.lu@ucl.ac.uk>
2024-11-10 10:53:57 +0000

    Add Vigour and PIT sanity check and support for 6.01 version

--- experiment_management/pilot6.jl
+++ experiment_management/pilot6.jl
@@ -1,8 +1,20 @@
 ### A Pluto.jl notebook ###
-# v0.20.1
+# v0.20.3
 
 using Markdown
 using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    #! format: off
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+    #! format: on
+end
 
 # ╔═╡ 237a05f6-9e0e-11ef-2433-3bdaa51dbed4
 begin
@@ -14,6 +26,7 @@ begin
     Pkg.instantiate()
 	using Random, DataFrames, JSON, CSV, StatsBase, JLD2, HTTP, CairoMakie, Printf, Distributions, CategoricalArrays, AlgebraOfGraphics, Dates
 	using LogExpFunctions: logistic, logit
+	using Tidier
 	include("fetch_preprocess_data.jl")
 	include("sample_utils.jl")
 	include("plotting_utils.jl")
@@ -42,6 +55,50 @@ begin
 	)
 	
 	set_theme!(th)
+end
+
+# ╔═╡ fa149033-bed0-41c8-898b-18bcbd1e34d7
+function load_pilot6_data(; force_download = false)
+	datafile = "data/pilot6.jld2"
+
+	# Load data or download from REDCap
+	if !isfile(datafile) || force_download
+		jspsych_json, records = get_REDCap_data("pilot6"; file_field = "file_data")
+	
+		jspsych_data = REDCap_data_to_df(jspsych_json, records)
+
+		filter!(x -> x.version ∈ ["6.0", "6.01"], jspsych_data)
+
+		remove_testing!(jspsych_data)
+
+		JLD2.@save datafile jspsych_data
+	else
+		JLD2.@load datafile jspsych_data
+	end
+
+	# Exctract PILT
+	PILT_data = prepare_PLT_data(jspsych_data; trial_type = "PILT")
+
+	# Divide intwo WM and PILT
+	WM_data = filter(x -> x.n_stimuli == 3, PILT_data)
+	filter!(x -> x.n_stimuli == 2, PILT_data)
+
+	# Extract post-PILT test
+	test_data = prepare_post_PILT_test_data(jspsych_data)
+
+	# Exctract vigour
+	vigour_data = prepare_vigour_data(jspsych_data) 
+
+	# Extract post-vigour test
+	post_vigour_test_data = prepare_post_vigour_test_data(jspsych_data)
+
+	# Extract PIT
+	PIT_data = prepare_PIT_data(jspsych_data)
+
+	# Exctract reversal
+	reversal_data = prepare_reversal_data(jspsych_data)
+
+	return PILT_data, test_data, vigour_data, post_vigour_test_data, PIT_data, WM_data, reversal_data, jspsych_data
 end
 
 # ╔═╡ d5811081-d5e2-4a6e-9fc9-9d70332cb338
@@ -388,6 +445,107 @@ let
 	f = draw(mp; axis = (; xticks = -3:9))
 end
 
+# ╔═╡ 7559e78d-7bd8-4450-a215-d74a0b1d670a
+md"""
+### Vigour
+"""
+
+# ╔═╡ 0312ce5f-be36-4d9b-aee3-04497f846537
+let
+	n_miss_df = @chain vigour_data begin
+		@mutate(pig = "Mag " * string(magnitude) * ", FR " * string(ratio))
+		@group_by(prolific_pid, pig)
+		@summarize(n_miss = sum(trial_presses == 0))
+		@ungroup()
+		@mutate(short_id = last.(prolific_pid, 5))
+	end
+	# Create the plot
+	plot = data(n_miss_df) * 
+		   mapping(:n_miss, layout=:pig) * 
+		   histogram(bins=5)
+	
+	# Set up the axis
+	axis = (
+		xlabel = "# No-response trials",
+		ylabel = "Count (# participants)"
+	)
+	
+	# Draw the plot
+	draw(plot; axis, figure=(;title="No-response trial distribution in Vigour task"))
+end
+
+# ╔═╡ 3d05e879-aa5c-4840-9f4f-ad35b8d9519a
+let
+	test_acc_df = @chain post_vigour_test_data begin
+		@mutate(
+			diff_rpp = (left_magnitude/left_ratio) - (right_magnitude/right_ratio),
+			chose_left = Int(response === "ArrowLeft")
+		)
+		@group_by(prolific_pid, version)
+		@select(diff_rpp, chose_left)
+		@mutate(chose_left = chose_left * 2 - 1, truth = sign(diff_rpp))
+		@mutate(acc = chose_left == truth)
+		@summarize(acc = mean(acc))
+		@ungroup
+	end
+	@info "Vigour acc: $(round(mean(test_acc_df.acc); digits=2))"
+	data(test_acc_df) *
+	mapping(:acc) *
+	visual(Hist) |>
+	draw(;axis=(;xlabel="Accuracy",ylabel="Count (#Participant)"))
+end
+
+# ╔═╡ 665aa690-4f37-4a31-b87e-3b4aee66b3b1
+md"""
+### PIT
+"""
+
+# ╔═╡ 89258a40-d4c6-4831-8cf3-d69d984c4f6e
+let
+	n_miss_df =  @chain PIT_data begin
+		# @filter(coin != 0)
+		@arrange(prolific_pid, magnitude, ratio)
+		@mutate(pig = "Mag " * string(magnitude) * ", FR " * string(ratio))
+		@group_by(prolific_pid, pig)
+		@summarize(n_miss = sum(trial_presses == 0))
+		@ungroup()
+		@mutate(short_id = last.(prolific_pid, 5))
+	end
+	# Create the plot
+	plot = data(n_miss_df) * 
+		   mapping(:n_miss, layout=:pig) * 
+		   histogram(bins=5)
+	
+	# Set up the axis
+	axis = (
+		xlabel = "# No-response trials",
+		ylabel = "Count (# participants)"
+	)
+	
+	# Draw the plot
+	draw(plot; axis, figure=(;title="No-response trial distribution in PIT task"))
+end
+
+# ╔═╡ ffd08086-f12c-4b8a-afb6-435c8729241e
+let
+	PIT_acc_df = @chain test_data begin
+		@filter(block == "pavlovian")
+		@group_by(same_valence)
+		@summarize(acc = mean(skipmissing((magnitude_right > magnitude_left) == right_chosen)))
+		@ungroup
+	end
+	@info "PIT acc for NOT in the same valence: $(round(PIT_acc_df.acc[PIT_acc_df.same_valence.==false][1]; digits=2))"
+	@info "PIT acc for in the same valence: $(round(PIT_acc_df.acc[PIT_acc_df.same_valence.==true][1]; digits=2))"
+	@chain test_data begin
+		@filter(block == "pavlovian")
+		@group_by(prolific_pid, exp_start_time, same_valence)
+		@summarize(acc = mean(skipmissing((magnitude_right > magnitude_left) == right_chosen)))
+		@ungroup
+		data(_) * mapping(:same_valence => nonnumeric => "Same valence", :acc => "PIT test accuracy", color=:same_valence => nonnumeric => "Same valence") * visual(RainClouds)
+		draw()
+	end
+end
+
 # ╔═╡ 91f6a95c-4f2e-4213-8be5-3ca57861ed15
 """
     extract_debrief_responses(data::DataFrame) -> DataFrame
@@ -528,23 +686,168 @@ for r in eachrow(p_sum)
 	end
 end
 
+# ╔═╡ ce27b319-d728-46f5-aaf1-051fe252bf8b
+function avg_presses_w_fn(vigour_data::DataFrame, x_var::Vector{Symbol}, y_var::Symbol, grp_var::Union{Symbol,Nothing}=nothing)
+    # Define grouping columns
+    group_cols = grp_var === nothing ? [:prolific_pid, x_var...] : [:prolific_pid, grp_var, x_var...]
+    # Group and calculate mean presses for each participant
+    grouped_data = groupby(vigour_data, Cols(group_cols...)) |>
+                   x -> combine(x, y_var => mean => :mean_y) |>
+                        x -> sort(x, Cols(group_cols...))
+    # Calculate the average across all participants
+    avg_w_data = @chain grouped_data begin
+        @group_by(prolific_pid)
+        @mutate(sub_mean = mean(mean_y))
+        @ungroup
+        @mutate(grand_mean = mean(mean_y))
+        @mutate(mean_y_w = mean_y - sub_mean + grand_mean)
+        groupby(Cols(grp_var === nothing ? x_var : [grp_var, x_var...]))
+        @summarize(
+            n = n(),
+            avg_y = mean(mean_y),
+            se_y = std(mean_y_w) / sqrt(length(prolific_pid)))
+        @ungroup
+    end
+    return grouped_data, avg_w_data
+end
+
+# ╔═╡ 8f6d8e98-6d73-4913-a02d-97525176549a
+let
+	df = @chain PIT_data begin
+		# @filter(coin != 0)
+		@arrange(prolific_pid, magnitude, ratio)
+		@mutate(pig = "Mag " * string(magnitude) * ", FR " * string(ratio))
+		@mutate(pig=categorical(pig,levels=["Mag 2, FR 16","Mag 2, FR 8","Mag 5, FR 8","Mag 1, FR 1"]))
+	end
+	grouped_data, avg_w_data = avg_presses_w_fn(df, [:coin, :pig], :press_per_sec)
+	p = data(avg_w_data) *
+	mapping(:coin=>nonnumeric, :avg_y, col=:pig=>nonnumeric) *
+	(
+	visual(Lines, linewidth=1, color=:gray) +
+	visual(Errorbars, whiskerwidth=4) *
+	mapping(:se_y, color=:coin => nonnumeric) +
+	visual(Scatter) *
+	mapping(color=:coin => nonnumeric)
+	)
+	draw(p, scales(Color = (; palette=:PRGn_7)); axis=(;xlabel="Pavlovian stimuli (coin)", ylabel="Press/sec", width=150, height=150, xticklabelrotation=pi/4))
+end
+
+# ╔═╡ e3f88292-fdb9-4628-88ee-8d935f00a761
+function plot_presses_vs_var(vigour_data::DataFrame; x_var::Union{Symbol, Pair{Symbol, typeof(AlgebraOfGraphics.nonnumeric)}}=:reward_per_press, y_var::Symbol=:trial_presses, grp_var::Union{Symbol,Nothing}=nothing, xlab::Union{String,Missing}=missing, ylab::Union{String,Missing}=missing, grplab::Union{String,Missing}=missing, combine::Bool=false)
+    plain_x_var = isa(x_var, Pair) ? x_var.first : x_var
+    grouped_data, avg_w_data = avg_presses_w_fn(vigour_data, [plain_x_var], y_var, grp_var)
+
+	# Set up the legend title
+    grplab_text = ismissing(grplab) ? uppercasefirst(join(split(string(grp_var), r"\P{L}+"), " ")) : grplab
+	
+    # Define mapping based on whether grp_var is provided
+    individual_mapping = grp_var === nothing ?
+                         mapping(x_var, :mean_y, group=:prolific_pid) :
+                         mapping(x_var, :mean_y, color=grp_var => grplab_text, group=:prolific_pid)
+
+    average_mapping = grp_var === nothing ?
+                      mapping(x_var, :avg_y) :
+                      mapping(x_var, :avg_y, color=grp_var => grplab_text)
+
+    # Create the plot for individual participants
+    individual_plot = data(grouped_data) *
+                      individual_mapping *
+                      visual(Lines, alpha=0.15, linewidth=1)
+
+    # Create the plot for the average line
+    if grp_var === nothing
+        average_plot = data(avg_w_data) *
+                       average_mapping * (
+                           visual(Errorbars, whiskerwidth=4) *
+                           mapping(:se_y) +
+                           visual(ScatterLines, linewidth=2)) *
+                       visual(color=:dodgerblue)
+    else
+        average_plot = data(avg_w_data) *
+                       average_mapping * (
+                           visual(Errorbars, whiskerwidth=4) *
+                           mapping(:se_y, color=grp_var => grplab_text) +
+                           visual(ScatterLines, linewidth=2))
+    end
+
+    # Combine the plots
+    fig = Figure(
+        size=(12.2, 7.6) .* 144 ./ 2.54, # 144 points per inch, then cm
+    )
+
+    # Set up the axis
+    xlab_text = ismissing(xlab) ? uppercasefirst(join(split(string(x_var), r"\P{L}+"), " ")) : xlab
+    ylab_text = ismissing(ylab) ? uppercasefirst(join(split(string(y_var), r"\P{L}+"), " ")) : ylab
+
+    if combine
+        axis = (;
+            xlabel=xlab_text,
+            ylabel=ylab_text,
+        )
+        final_plot = individual_plot + average_plot
+        fig = draw(final_plot; axis=axis)
+    else
+        # Draw the plot
+        fig_patch = fig[1, 1] = GridLayout()
+        ax_left = Axis(fig_patch[1, 1], ylabel=ylab_text)
+        ax_right = Axis(fig_patch[1, 2])
+        Label(fig_patch[2, :], xlab_text)
+        draw!(ax_left, individual_plot)
+        f = draw!(ax_right, average_plot)
+        legend!(fig_patch[1, 3], f)
+        rowgap!(fig_patch, 5)
+    end
+    return fig
+end
+
+# ╔═╡ 814aec54-eb08-4627-9022-19f41bcdac9f
+let
+	transform!(vigour_data, [:trial_presses, :trial_duration] => ((x, y) -> x .* 1000 ./ y) => :press_per_sec)
+	plot_presses_vs_var(vigour_data; x_var=:reward_per_press, y_var=:press_per_sec, grp_var=:trialphase, xlab="Reward/press", ylab = "Press/sec", combine=false)
+end
+
+# ╔═╡ a6794b95-fe5e-4010-b08b-f124bff94f9f
+let
+	common_rpp = unique(PIT_data.reward_per_press)
+	transform!(PIT_data, [:trial_presses, :trial_duration] => ((x, y) -> x .* 1000 ./ y) => :press_per_sec)
+	instrumental_data = @chain PIT_data begin
+		# @filter(coin==0)
+		@bind_rows(vigour_data)
+		@mutate(trialphase=categorical(trialphase, levels=["vigour_trial", "pit_trial"], ordered=true))
+		@filter(reward_per_press in !!common_rpp)
+	end
+	plot_presses_vs_var(instrumental_data; x_var=:reward_per_press, y_var=:press_per_sec, grp_var=:trialphase, xlab="Reward/press", ylab = "Press/sec", combine=false)
+end
+
 # ╔═╡ Cell order:
 # ╠═237a05f6-9e0e-11ef-2433-3bdaa51dbed4
 # ╠═0d120e19-28c2-4a98-b873-366615a5f784
+# ╠═fa149033-bed0-41c8-898b-18bcbd1e34d7
 # ╟─d5811081-d5e2-4a6e-9fc9-9d70332cb338
 # ╠═36b348cc-a3bf-41e7-aac9-1f6d858304a2
 # ╠═c6d0d8c2-2c26-4e9c-8c1b-a9b23d985971
 # ╠═6ca0676f-b107-4cc7-b0d2-32cc345dab0d
 # ╟─cb4f46a2-1e9b-4006-8893-6fc609bcdf52
 # ╟─5d487d8d-d494-45a7-af32-7494f1fb70f2
-# ╠═2ff04c44-5f86-4617-9a13-6d4228dff359
+# ╟─2ff04c44-5f86-4617-9a13-6d4228dff359
 # ╟─d0a2ba1e-8413-48f8-8bbc-542f3555a296
 # ╟─2897a681-e8dd-4091-a2a0-bd3d4cd23209
 # ╟─176c54de-e84c-45e5-872e-2471e575776d
 # ╟─18956db1-4ad1-4881-a1e7-8362cf59f011
-# ╠═18e9fccd-cc0d-4e8f-9e02-9782a03093d7
-# ╠═17666d61-f5fc-4a8d-9624-9ae79f3de6bb
+# ╟─18e9fccd-cc0d-4e8f-9e02-9782a03093d7
+# ╟─17666d61-f5fc-4a8d-9624-9ae79f3de6bb
 # ╟─1d1d6d79-5807-487f-8b03-efb7d0898ae8
 # ╟─e902cd57-f724-4c26-9bb5-1d03443fb191
-# ╠═dc957d66-1219-4a97-be46-c6c5c189c8ba
-# ╠═91f6a95c-4f2e-4213-8be5-3ca57861ed15
+# ╟─7559e78d-7bd8-4450-a215-d74a0b1d670a
+# ╟─0312ce5f-be36-4d9b-aee3-04497f846537
+# ╟─814aec54-eb08-4627-9022-19f41bcdac9f
+# ╟─3d05e879-aa5c-4840-9f4f-ad35b8d9519a
+# ╟─665aa690-4f37-4a31-b87e-3b4aee66b3b1
+# ╟─89258a40-d4c6-4831-8cf3-d69d984c4f6e
+# ╟─a6794b95-fe5e-4010-b08b-f124bff94f9f
+# ╟─8f6d8e98-6d73-4913-a02d-97525176549a
+# ╟─ffd08086-f12c-4b8a-afb6-435c8729241e
+# ╟─dc957d66-1219-4a97-be46-c6c5c189c8ba
+# ╟─91f6a95c-4f2e-4213-8be5-3ca57861ed15
+# ╟─ce27b319-d728-46f5-aaf1-051fe252bf8b
+# ╟─e3f88292-fdb9-4628-88ee-8d935f00a761
