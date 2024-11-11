e7e8698700a2eae42df1a8fd95a75f8e0e35b028
yanivabir <yanivabir@gmail.com>
2024-11-11 11:58:29 +0000

    acc curve

--- data_analysis/workshop/PILT.jl
+++ data_analysis/workshop/PILT.jl
@@ -48,6 +48,69 @@ begin
 	set_theme!(th)
 end
 
+# ╔═╡ 14a292db-43d4-45d8-97a5-37ffc03bdc5c
+begin
+	# Load data
+	PILT_data, _, _, _, _, _,
+		_, _ = load_pilot6_data()
+	nothing
+end
+
+# ╔═╡ 6ed82686-35ab-4afd-a1b2-6fa19ae67168
+PILT_data_clean = let
+	# Clean data
+	PILT_data_clean = exclude_PLT_sessions(PILT_data, required_n_blocks = 20)
+	PILT_data_clean = filter(x -> x.response != "noresp", PILT_data_clean)
+end
+
+# ╔═╡ b5b75f4e-7b91-4287-a409-6f0ebdf20f4e
+let
+
+	# Sumarrize by participant and trial
+	acc_curve = combine(
+		groupby(PILT_data_clean, [:prolific_pid, :trial]),
+		:response_optimal => mean => :acc
+	)
+
+	sort!(acc_curve, [:prolific_pid, :trial])
+
+	# Summarize by trial
+	acc_curve_sum = combine(
+		groupby(acc_curve, :trial),
+		:acc => mean => :acc,
+		:acc => sem => :se
+	)
+
+	# Compute bounds
+	insertcols!(
+		acc_curve_sum,
+		:lb => acc_curve_sum.acc .- acc_curve_sum.se,
+		:ub => acc_curve_sum.acc .+ acc_curve_sum.se
+	)
+
+	# Plot
+	mp = data(acc_curve_sum) * 
+	(
+	# Error band
+		mapping(
+		:trial => "Trial #",
+		:lb,
+		:ub
+	) * visual(Band) +
+	# Average line	
+		mapping(
+		:trial => "Trial #",
+		:acc => "Prop. optimal choice"
+	) * visual(Lines, linewidth = 4)
+	)
+	
+	
+	draw(mp; legend = (; show = false))
+end
+
 # ╔═╡ Cell order:
 # ╠═8cf30b5e-a020-11ef-23b2-2da6e9116b54
 # ╠═82ef300e-536f-40ce-9cde-72056e6f4b5e
+# ╠═14a292db-43d4-45d8-97a5-37ffc03bdc5c
+# ╠═6ed82686-35ab-4afd-a1b2-6fa19ae67168
+# ╠═b5b75f4e-7b91-4287-a409-6f0ebdf20f4e
