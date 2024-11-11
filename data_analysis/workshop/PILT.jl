7cc902b2176e71b0f0a4bd131ca60122e008ef49
yanivabir <yanivabir@gmail.com>
2024-11-11 12:53:14 +0000

    plot acc curve gradually, save to osf

--- data_analysis/workshop/PILT.jl
+++ data_analysis/workshop/PILT.jl
@@ -12,7 +12,7 @@ begin
     Pkg.activate("relmed_environment")
     # instantiate, i.e. make sure that all packages are downloaded
     Pkg.instantiate()
-	using Random, DataFrames, JSON, CSV, StatsBase, JLD2, HTTP, CairoMakie, Printf, Distributions, CategoricalArrays, AlgebraOfGraphics, Dates, Turing
+	using Random, DataFrames, JSON, CSV, StatsBase, JLD2, HTTP, CairoMakie, Printf, Distributions, CategoricalArrays, AlgebraOfGraphics, Dates, Turing, SHA
 	using LogExpFunctions: logistic, logit
 	import OpenScienceFramework as OSF
 	include("fetch_preprocess_data.jl")
@@ -46,6 +46,13 @@ begin
 		)
 	)
 	set_theme!(th)
+end
+
+# ╔═╡ 595c642e-32df-448e-81cc-6934e2152d70
+# Set up saving to OSF
+begin
+	osf_folder = "Workshop figures/PILT"
+	proj = setup_osf("Task development")
 end
 
 # ╔═╡ 14a292db-43d4-45d8-97a5-37ffc03bdc5c
@@ -88,14 +95,13 @@ let
 		:ub => acc_curve_sum.acc .+ acc_curve_sum.se
 	)
 
-	# Plot
-	mp = data(acc_curve_sum) * 
-	(
+	# Create plot mapping
+	mp = (
 	# Error band
 		mapping(
 		:trial => "Trial #",
-		:lb,
-		:ub
+		:lb => "Prop. optimal choice",
+		:ub => "Prop. optimal choice"
 	) * visual(Band) +
 	# Average line	
 		mapping(
@@ -103,14 +109,58 @@ let
 		:acc => "Prop. optimal choice"
 	) * visual(Lines, linewidth = 4)
 	)
+
+	# Mathcing line
+	matching = mapping([0.86]) * visual(HLines, linestyle = :dash)
+
+	# Plot whole data
+	f1 = Figure()
 	
+	draw!(f1, data(acc_curve_sum) * mp + matching)
+
+	# Plot up to trial 5
+	f2 = Figure()
 	
-	draw(mp; legend = (; show = false))
+	draw!(
+		f2, 
+		data(filter(x -> x.trial <= 5, acc_curve_sum)) * mp)
+
+
+	# Plot with matching level
+	f3 = Figure()
+	
+	draw!(
+		f3, 
+		data(filter(x -> x.trial <= 5, acc_curve_sum)) * 
+		mp + 
+		matching
+	)
+
+	# Link axes
+	linkaxes!(contents(f1[1,1])[1], contents(f2[1,1])[1], contents(f3[1,1])[1])
+
+	# Save
+	filepaths = [joinpath("results/workshop", "PILT_acc_curve_$k.png") for k in ["full", "partial", "partial_line"]]
+
+	save.(filepaths, [f1, f2, f3])
+
+	for fp in filepaths
+		upload_to_osf(
+			fp,
+			proj,
+			osf_folder
+		)
+	end
+
+
+	# f1, f2, f3
+	
 end
 
 # ╔═╡ Cell order:
 # ╠═8cf30b5e-a020-11ef-23b2-2da6e9116b54
 # ╠═82ef300e-536f-40ce-9cde-72056e6f4b5e
+# ╠═595c642e-32df-448e-81cc-6934e2152d70
 # ╠═14a292db-43d4-45d8-97a5-37ffc03bdc5c
 # ╠═6ed82686-35ab-4afd-a1b2-6fa19ae67168
 # ╠═b5b75f4e-7b91-4287-a409-6f0ebdf20f4e
