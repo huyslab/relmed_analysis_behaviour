5975aedb56d77e7a8e496d7054c774df500f343a
yanivabir <yanivabir@gmail.com>
2024-11-15 11:12:22 +0000

    reliability of first trial generalization effect in PILT

--- data_analysis/workshop/category_generalization.jl
+++ data_analysis/workshop/category_generalization.jl
@@ -165,6 +165,7 @@ PILT_data_clean = let
 end
 
 # ╔═╡ 3cb8fb6f-7457-499d-9aad-47e0f2e8ec5c
+# Plot first trial in PILT
 let
 
 	# Summarize repeating_chosen on 1st trial by participant, previous valence, repeating_previous_optimal
@@ -227,6 +228,152 @@ let
 
 end
 
+# ╔═╡ 4f4995a7-0582-43d8-899d-0ed899d8fa73
+# Reliability of first trial in PILT
+let 
+	fs = []
+
+	# Run over sessions and parameters
+	for s in unique(PILT_data_clean.session)
+
+		# Select data and add half variable
+		first_trial = DataFrames.transform!(
+			filter(x -> !ismissing(x.repeating_chosen) && (x.trial == 1) && 
+				(x.session == s),
+				PILT_data_clean),
+			:block => (x -> ifelse.(
+				x .< (median(unique(x)) + 2),
+				fill(1, length(x)),
+				fill(2, length(x))
+			)) => :half
+		) 
+		
+		# Summarize repeating_chosen on 1st trial by participant, half, repeating_previous_optimal
+		repeat_sum = combine(
+			groupby(
+				first_trial, 
+				[:prolific_pid, :half, :repeating_previous_optimal]),
+			:repeating_chosen => mean => :repeating_chosen
+		)
+	
+		# Long to wide
+		repeat_sum = unstack(
+			repeat_sum,
+			[:prolific_pid, :half],
+			:repeating_previous_optimal,
+			:repeating_chosen
+		)
+	
+		repeat_sum.diff = repeat_sum[!, Symbol("true")] .- repeat_sum[!, Symbol("false")]
+	
+		# Long to wide
+		repeat_sum = unstack(
+			repeat_sum,
+			[:prolific_pid],
+			:half,
+			:diff,
+			renamecols = x -> "half_$x"
+		)
+	
+		#Plot
+		f = Figure()
+		workshop_reliability_scatter!(
+			f[1, 1];
+			df = repeat_sum,
+			xcol = :half_1,
+			ycol = :half_2,
+			xlabel = "First half",
+			ylabel = "Second half",
+			subtitle = "Session $s optimality generalization effect"
+		)
+
+		# Save plot
+		filepath = "results/workshop/generalization_PILT_sess$(s)_1st_trial_optimality_splithalf.png"
+	
+		save(filepath, f)
+	
+		# upload_to_osf(
+		# 		filepath,
+		# 		proj,
+		# 		osf_folder
+		# )
+
+		push!(fs, f)
+	end
+
+	fs[1]
+
+end
+
+# ╔═╡ 69fd4fa3-f5da-4d63-b132-e3b2903293dd
+# ╠═╡ disabled = true
+#=╠═╡
+# Test-retest reliability of first trial in PILT
+let
+	# Select data and add half variable
+	first_trial = DataFrames.transform!(
+		filter(x -> !ismissing(x.repeating_chosen) && (x.trial == 1),
+			PILT_data_clean),
+		:block => (x -> ifelse.(
+			x .< (median(unique(x)) + 2),
+			fill(1, length(x)),
+			fill(2, length(x))
+		)) => :half
+	) 
+	
+	# Summarize repeating_chosen on 1st trial by participant, half, repeating_previous_optimal
+	repeat_sum = combine(
+		groupby(
+			first_trial, 
+			[:prolific_pid, :session, :repeating_previous_optimal]),
+		:repeating_chosen => mean => :repeating_chosen
+	)
+
+	# Long to wide
+	repeat_sum = unstack(
+	repeat_sum,
+	[:prolific_pid, :session],
+	:repeating_previous_optimal,
+	:repeating_chosen
+	)
+	
+	repeat_sum.diff = repeat_sum[!, Symbol("true")] .- repeat_sum[!, Symbol("false")]
+	
+	# Long to wide
+	repeat_sum = unstack(
+		repeat_sum,
+		[:prolific_pid],
+		:session,
+		:diff,
+		renamecols = x -> "sess_$x"
+	)
+	
+	#Plot
+	f = Figure()
+	workshop_reliability_scatter!(
+		f[1, 1];
+		df = repeat_sum,
+		xcol = :sess_1,
+		ycol = :sess_2,
+		xlabel = "Session 1",
+		ylabel = "Session 2",
+		subtitle = "Optimality generalization effect"
+	)
+	
+	# Save plot
+	filepath = "results/workshop/generalization_PILT_1st_trial_optimality_test_retest.png"
+	
+	save(filepath, f)
+	
+	# upload_to_osf(
+	# 		filepath,
+	# 		proj,
+	# 		osf_folder
+	# )
+
+end
+  ╠═╡ =#
+
 # ╔═╡ Cell order:
 # ╠═c4f778a8-a207-11ef-1db0-f57fc0a2a769
 # ╠═d6f8130a-3527-4c89-aff2-0c0e64d494d9
@@ -234,3 +381,5 @@ end
 # ╠═0f1cf0ad-3a49-4c8e-8b51-607b7237e02f
 # ╠═3b019f83-64f3-428a-96d6-42d9cc1969fd
 # ╠═3cb8fb6f-7457-499d-9aad-47e0f2e8ec5c
+# ╠═4f4995a7-0582-43d8-899d-0ed899d8fa73
+# ╠═69fd4fa3-f5da-4d63-b132-e3b2903293dd
