7507e49d7e76f360146d924582e0a0fef98287fc
yanivabir <yanivabir@gmail.com>
2025-02-28 11:27:02 +0000

    test performance by value

--- data_analysis/pilot7_WM_data_plots.jl
+++ data_analysis/pilot7_WM_data_plots.jl
@@ -50,6 +50,12 @@ begin
 	_, test_data, _, _, _, WM_data, _, _ = load_pilot7_data(; force_download = false, return_version = "7.0")
 end
 
+# ╔═╡ f081f2d7-bef3-476c-ad14-ebc4f0a35e77
+md"""## Test Performance"""
+
+# ╔═╡ 6ab2f6d7-186d-435d-9bc1-a50b079e4eda
+filter(x -> x.block == 6, test_data)
+
 # ╔═╡ 01255688-ee82-4f77-95ba-5fb2c1d5a717
 	function equi_groups(x::AbstractVector; n::Int = 3, labels = ["Early", "Mid", "Late"])
 	    min_x, max_x = extrema(x)
@@ -415,6 +421,157 @@ let df = WM_data_clean
 	
 end
 
+# ╔═╡ 0da6af42-ef20-4d28-8818-92abd077ecf7
+value_sum = let df = WM_data_clean
+
+	value_sum = vcat(
+		[select(
+			df,
+			:prolific_pid,
+			:stimulus_group_id,
+			Symbol("stimulus_$side") => :stimulus,
+			Symbol("feedback_$side") => :feedback
+		) for side in ["left", "middle", "right"]]...
+	)
+
+	value_sum = combine(
+		groupby(
+			value_sum,
+			[:prolific_pid, :stimulus_group_id, :stimulus]),
+		:feedback => mean => :value
+	)
+
+end
+
+# ╔═╡ 9382247e-d825-4e3f-89f8-959683b0fe62
+value_test_data = let vdf = value_sum,
+	tdf = filter(x -> x.block == 6, test_data)
+
+	# Select relevant columns
+	select!(
+		tdf,
+		:prolific_pid,
+		:trial,
+		:stimulus_left,
+		:stimulus_right,
+		:right_chosen
+	)
+
+	# Merge values from learning phase
+	for side in ["left", "right"]
+	
+		leftjoin!(
+			tdf,
+			select(
+				vdf,
+				:prolific_pid,
+				:stimulus => Symbol("stimulus_$side"),
+				:value => Symbol("value_$side")
+			),
+			on = [:prolific_pid, Symbol("stimulus_$side")]
+		)
+	end
+
+	# Whether high value chosen
+	tdf.high_chosen = ifelse.(
+		tdf.value_right .> tdf.value_left,
+		tdf.right_chosen,
+		.!tdf.right_chosen
+	)
+
+	# Value difference
+	tdf.Δ_val = tdf.value_right - tdf.value_left
+
+	tdf
+
+end
+
+# ╔═╡ 7b97683e-ba34-4e48-8fca-fd863dde343e
+let tdf = value_test_data
+	f = Figure()
+
+	# Histogram of values --------------------
+	ax = Axis(
+		f[1,1],
+		yautolimitmargin = (0,0),
+		xlabel = "Average observed outcome",
+		xticks = [0.01, 0.5, 0.75, 1.]
+	)
+	
+	hist!(ax, value_sum.value; bins = vcat([0.01 - 0.025 / 2, 0.01 + 0.025 / 2], range(0.75, 1., 11)), color = :grey)
+
+	hideydecorations!(ax)
+
+	hidespines!(ax, :l)
+
+	# Choice by value ----------------------
+
+	# Remove missing
+	dropmissing!(tdf)
+
+	# Bin value
+	vbins = vcat(
+		[-1., -0.8],
+		range(-0.2, 0.2, 6),
+		[0.8, 1.]
+	)
+
+	vlabels = [mean([vbins[i], vbins[i+1]]) for i in 1:(length(vbins)-1)]
+
+	tdf.Δ_val_bin = recoder(
+		tdf.Δ_val,
+		vbins[2:end],
+		vlabels
+	)
+
+	# Average by participant
+	tsum = combine(
+		groupby(
+			tdf,
+			[:prolific_pid, :Δ_val_bin]),
+		:right_chosen => mean => :right_chosen
+	)
+
+	# Average across participants
+	tsum = combine(
+		groupby(
+			tsum,
+			:Δ_val_bin),
+		:right_chosen => mean => :right_chosen,
+		:right_chosen => sem => :se,
+		:right_chosen => length => :n
+	)
+
+	# Remove rare
+	filter!(x -> x.n > 10, tsum)
+
+	# Plot
+	vmap = data(tsum) * (mapping(
+		:Δ_val_bin,
+		:right_chosen,
+		:se
+	) * visual(Errorbars) + mapping(
+		:Δ_val_bin,
+		:right_chosen
+	) * visual(Scatter))
+
+	draw!(
+		f[1,2],
+		vmap;
+		axis = (;
+			xlabel = "Δ average outcome\nright − left",
+			ylabel = "Prop. right chosen",
+			xticks = sort(unique(tsum.Δ_val_bin)),
+			xticklabelrotation = 45,
+			limits = (nothing, (0., 1.))
+		)
+	)
+	
+
+	f
+
+end
+
 # ╔═╡ Cell order:
 # ╠═fb93a03b-8ae6-4395-bae3-4183fbe45cc5
 # ╠═b4067011-f52b-4da7-a278-063d8743bcaf
@@ -425,6 +582,11 @@ end
 # ╠═14a4593b-9fd0-4d2f-964f-32d5b1530067
 # ╠═88591a1a-f440-11ef-2b4f-9f1d7635cbe4
 # ╠═e72ee428-01bc-4cab-aa4c-1de1d7e60693
+# ╟─f081f2d7-bef3-476c-ad14-ebc4f0a35e77
+# ╠═0da6af42-ef20-4d28-8818-92abd077ecf7
+# ╠═9382247e-d825-4e3f-89f8-959683b0fe62
+# ╠═7b97683e-ba34-4e48-8fca-fd863dde343e
+# ╠═6ab2f6d7-186d-435d-9bc1-a50b079e4eda
 # ╠═01255688-ee82-4f77-95ba-5fb2c1d5a717
 # ╠═ff992cf9-e256-4ebc-a661-e13780cb99a0
 # ╠═8f52df1d-598a-43e3-bd07-2e2bdf458c93
