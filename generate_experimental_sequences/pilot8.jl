26bfab3f0d7fca2ff919ff2d276e1e131238869d
yanivabir <yanivabir@gmail.com>
2025-03-04 15:22:52 +0000

    RLWM sequence

--- generate_experimental_sequences/pilot8.jl
+++ generate_experimental_sequences/pilot8.jl
@@ -60,9 +60,6 @@ categories = let
 	categories
 end
 
-# ╔═╡ bac8df40-6ba8-4b52-9bc5-306935963179
-
-
 # ╔═╡ 54f6f217-3ae5-49c7-9456-a5abcbbdc62f
 # RLX Parameters
 begin
@@ -83,11 +80,11 @@ triplet_order = let
 	)
 end
 
+# ╔═╡ 184a054c-5a88-44f8-865e-da75a10191ec
+md"""## RLWM"""
+
 # ╔═╡ ffe06202-d829-4145-ae26-4a95449d64e6
 md"""# RLLTM"""
-
-# ╔═╡ 6eadaa9d-5ed0-429b-b446-9cc7fbfb52dc
-length(categories)
 
 # ╔═╡ 3fa8c293-ac47-4acd-bdb7-9313286ee464
 function assign_triplet_stimuli_RLLTM(
@@ -151,9 +148,9 @@ function prop_fill_shuffle(
 	shuffle(rng, vcat(res...))
 end
 
-# ╔═╡ f5916a9f-ddcc-4c03-9328-7dd76c4c74b2
-# Create deterministic block
-RLLTM_det_block = let rng = Xoshiro(3)
+# ╔═╡ b9134153-d9e9-4e35-bfc4-2c5c5a4329ee
+RLX_block = let rng = Xoshiro(0)
+
 	n_trials = nrow(triplet_order)
 
 	# Basic variables
@@ -178,6 +175,147 @@ RLLTM_det_block = let rng = Xoshiro(3)
 
 	@info "Proportion fifty pence: $(mean(det_block.feedback_optimal .== 0.5))"
 
+	det_block
+
+end
+
+# ╔═╡ 6417ad94-1852-4cce-867e-a856295ec782
+# Create deterministic block
+RLWM = let RLWM = copy(RLX_block),
+	rng = Xoshiro(0)
+
+	# Assign stimuli --------
+	stimuli = [
+		categories[rand(rng, 1:length(categories))] * "_1.jpg" for _ in 1:length(unique(RLWM.stimulus_group))
+	]
+
+	stimuli = DataFrame(
+		stimulus_group = unique(RLWM.stimulus_group),
+		stimulus_left = stimuli
+	)
+	
+		
+	leftjoin!(
+		RLWM,
+		stimuli,
+		on = :stimulus_group
+	)
+
+	# Replicate - for RLWM there is only one stimulus, but this is requirement of js script
+	RLWM.stimulus_middle = RLWM.stimulus_left
+	RLWM.stimulus_right = RLWM.stimulus_left
+	
+	# Assign stimuli locations -----------------------------
+	# Count appearances per stimulus_group
+	stimulus_ordering = combine(
+		groupby(RLWM, [:stimulus_group]),
+		:stimulus_group => length => :n
+	)
+
+	# Sort by descending n to distribute largest trials first
+	shuffle!(rng, stimulus_ordering)
+	sort!(stimulus_ordering, :n, rev=true)
+
+	# Track total counts per action
+	action_sums = Dict(1 => 0, 2 => 0, 3 => 0)
+
+	# Place holder for optimal action
+	stimulus_ordering.optimal_action .= 99
+	
+	# Assign actions to balance total n
+	for row in eachrow(stimulus_ordering)
+	    # Pick the action with the smallest current total
+	    best_action = argmin(action_sums)
+	    row.optimal_action = best_action
+	    action_sums[best_action] += row.n
+	end
+
+
+	# Join with data frame
+	leftjoin!(
+		RLWM,
+		select(stimulus_ordering, [:stimulus_group, :optimal_action]),
+		on = :stimulus_group
+	)
+
+	@info "Proportion optimal in each location: $([round(mean((x -> x == i).(RLWM.optimal_action)), digits = 3) for i in 1:3])"
+
+	# Additional variables --------
+
+	# Assign feedback to action
+	for (i, side) in enumerate(["left", "middle", "right"])
+		RLWM[!, Symbol("feedback_$side")] = ifelse.(
+			(x -> x == i).(RLWM.optimal_action),
+			RLWM.feedback_optimal,
+			fill(0.01, nrow(RLWM))
+		)
+	end
+
+	# For compatibility with probabilistic block
+	RLWM.feedback_common .= true
+
+	# Session variable
+	RLWM.session .= 1
+
+	# Valence variable
+	RLWM.valence .= 1
+
+	# Apperance variable
+	DataFrames.transform!(
+		groupby(RLWM, [:block, :stimulus_group]),
+		:trial => (x -> 1:length(x)) => :appearance
+	)
+
+	# Cumulative triplet index
+	RLWM.stimulus_group_id = RLWM.stimulus_group .+ (maximum(RLWM.stimulus_group) .* (RLWM.block .- 1))
+
+	# Create optimal_side variable
+	RLWM.optimal_side = (x -> ["left", "middle", "right"][x]).(RLWM.optimal_action)
+		
+
+	# Add variables needed for experiment code
+	insertcols!(
+		RLWM,
+		:n_stimuli => 1,
+		:optimal_right => "",
+		:present_pavlovian => false,
+		:n_groups => maximum(RLWM.stimulus_group),
+		:early_stop => false
+	)
+
+
+	RLWM
+
+end
+
+# ╔═╡ e6f984aa-20dc-4a7d-8a3b-75728995a1f7
+# Checks
+let task = RLWM
+	@assert all(combine(groupby(task, [:session]), 
+		:block => issorted => :sorted).sorted) "Task structure not sorted by block"
+
+	@assert all(combine(groupby(task, [:session, :block]), 
+		:trial => issorted => :sorted).sorted) "Task structure not sorted by trial number"
+	
+	@assert all(sign.(task.valence) == sign.(task.feedback_right)) "Valence doesn't match feedback sign"
+
+	@assert all(sign.(task.valence) == sign.(task.feedback_left)) "Valence doesn't match feedback sign"
+
+	@info "Overall proportion of common feedback: $(round(mean(task.feedback_common), digits = 2))"
+
+end
+
+# ╔═╡ efdfdeb0-2b56-415e-acf7-d6236ee7b199
+let
+	save_to_JSON(RLWM, "results/pilot8_WM.json")
+	CSV.write("results/pilot8_WM.csv", RLWM)
+end
+
+# ╔═╡ f5916a9f-ddcc-4c03-9328-7dd76c4c74b2
+# Create deterministic block
+RLLTM_det_block = let det_block = copy(RLX_block),
+	rng = Xoshiro(0)
+	
 	# Assign stimuli categories
 	stimuli = assign_triplet_stimuli_RLLTM((categories),
 		maximum(det_block.stimulus_group);
@@ -200,6 +338,7 @@ RLLTM_det_block = let rng = Xoshiro(3)
 	)
 
 	# Sort by descending n to distribute largest trials first
+	shuffle!(rng, stimulus_ordering)
 	sort!(stimulus_ordering, :n, rev=true)
 
 	# Track total counts per action
@@ -324,6 +463,10 @@ let task = RLLTM
 
 	@info "Overall proportion of common feedback: $(round(mean(task.feedback_common), digits = 2))"
 
+	@assert !all(RLWM.feedback_left .== RLLTM.feedback_left) "Optimal actions should be different on RLWM and RLLTM"
+
+	@assert all(RLWM.stimulus_group .== RLLTM.stimulus_group) "Stimuli sequence not identical in RLWM and RLLTM"
+
 end
 
 # ╔═╡ eecaac0c-e051-4543-988c-e969de3a8567
@@ -336,12 +479,15 @@ end
 # ╠═2d7211b4-b31e-11ef-3c0b-e979f01c47ae
 # ╠═114f2671-1888-4b11-aab1-9ad718ababe6
 # ╠═de74293f-a452-4292-b5e5-b4419fb70feb
-# ╠═bac8df40-6ba8-4b52-9bc5-306935963179
 # ╠═54f6f217-3ae5-49c7-9456-a5abcbbdc62f
 # ╠═9f300301-b018-4bea-8fc4-4bc889b11afd
+# ╠═b9134153-d9e9-4e35-bfc4-2c5c5a4329ee
+# ╟─184a054c-5a88-44f8-865e-da75a10191ec
+# ╠═6417ad94-1852-4cce-867e-a856295ec782
+# ╠═e6f984aa-20dc-4a7d-8a3b-75728995a1f7
+# ╠═efdfdeb0-2b56-415e-acf7-d6236ee7b199
 # ╟─ffe06202-d829-4145-ae26-4a95449d64e6
 # ╠═f5916a9f-ddcc-4c03-9328-7dd76c4c74b2
-# ╠═6eadaa9d-5ed0-429b-b446-9cc7fbfb52dc
 # ╠═e3bff0b9-306a-4bf9-8cbd-fe0e580bd118
 # ╠═f9be1490-8e03-445f-b36e-d8ceff894751
 # ╠═eecaac0c-e051-4543-988c-e969de3a8567
