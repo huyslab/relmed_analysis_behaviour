597e43e9573c771842a0d02b63fb27ef624d6dfa
Haoyang Lu <haoyang.lu@ucl.ac.uk>
2025-03-06 21:46:41 +0000

    Test simulation with slower keypresser

--- control_task/simulation/controltask_simulation.jl
+++ control_task/simulation/controltask_simulation.jl
@@ -3,7 +3,7 @@ Pkg.activate("relmed_environment")
 Pkg.instantiate()
 @info "Running with $(Threads.nthreads()) threads"
 
-using Revise, Random, CairoMakie
+using Revise, Random, DataFrames, CairoMakie, Distributions, CSV, JSON
 CairoMakie.activate!(type = "svg")
 includet("ControlEffortTask.jl")
 using .ControlEffortTask
@@ -11,6 +11,7 @@ using .ControlEffortTask
 pars = TaskParameters(
      n_trials=144,
      beta_true=[2, 5, 8] .* 3,
+     n_particles=40
 )
 
 # Set random seed for reproducibility
@@ -22,4 +23,13 @@ Random.seed!(123)
 
 # Run batch experiments
 stimuli_sequence = generate_stimuli(pars; mode=:factorial, shuffle=true)
-results_list, datasets_list, batch_fig = run_batch_experiment(pars, stimuli_sequence; n_participants=100, show_plots=true);
+
+# results_list, datasets_list, batch_fig = run_batch_experiment(pars, stimuli_sequence; n_participants=100, show_plots=true);
+
+# Weaker actors
+function actor_fn(stimulus) 
+     chosen_boat = stimulus.boats[rand(1:2)]
+     effort = rand(DiscreteUniform(0, 25))
+     return (; chosen_boat, effort)
+end
+results_list, datasets_list, batch_fig = run_batch_experiment(pars, stimuli_sequence; n_participants=50, actor = actor_fn, show_plots=true);
