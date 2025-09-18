39da7501e921b7765ab5f567c8551ae9e73be82e
yanivabir <yanivabir@gmail.com>
2025-09-18 15:06:07 +0000

    feat: experiment registry

---
+++ core/experiment-registry.jl
@@ -0,0 +1,20 @@
+# This file contains a registry of all RELMED pilot and trial experiments, with information needed to load and analyse data.
+
+"""
+Represents metadata for a single experiment.
+"""
+struct ExperimentInfo
+    project::String
+    tasks_included::Vector{String}
+    participant_id_field::String
+    date_collected::Union{Date, Nothing}
+    notes::Union{String, Nothing}
+end
+
+trial1 = ExperimentInfo(
+    "trial1",
+    ["reversal", "max_press", "PILT", "vigour", "PIT", "vigour_test", "post_PILT_test", "control", "WM", "delay_discounting", "open_text"],
+    "participant_id",
+    Date(2025, 6, 4),
+    "First RELMED trial with participants."
+)
