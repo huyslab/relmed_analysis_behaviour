aee869e06341c3f801ddb7ec14615b855613f73b
yanivabir <yanivabir@gmail.com>
2025-09-18 14:23:32 +0000

    feat: config file for figures

---
+++ projects/behaviour-dashboard/config.jl
@@ -0,0 +1,26 @@
+# Plotting configurations for behaviour dashboard
+
+# Plotting theme
+th = Theme(
+    font = "Helvetica",
+    fontsize = 16,
+    Axis = (
+        xgridvisible = false,
+        ygridvisible = false,
+        rightspinevisible = false,
+        topspinevisible = false,
+        xticklabelsize = 14,
+        yticklabelsize = 14,
+        spinewidth = 1.5,
+        xtickwidth = 1.5,
+        ytickwidth = 1.5
+    )
+)
+
+set_theme!(th)
+
+# Plotting configurations
+configs = Dict(
+    
+)
+
