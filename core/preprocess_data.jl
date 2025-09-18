3a3f38fa03fd56451795c1400c5919fb6bb0501f
yanivabir <yanivabir@gmail.com>
2025-09-18 21:14:44 +0000

    feat: more task preproc funcs

--- core/preprocess_data.jl
+++ core/preprocess_data.jl
@@ -1,38 +1,118 @@
 # Preprocess data from REDCap, dividing into tasks and preparing various variables
+using DataFrames, JLD2, JSON
+include("$(pwd())/core/fetch_redcap.jl")
+include("$(pwd())/core/experiment-registry.jl")
 
 remove_testing!(data::DataFrame; participant_id_column::Symbol = :participant_id) = filter!(x -> (!occursin(r"haoyang|yaniv|tore|demo|simulate|debug", x[participant_id_column])) && (length(x[participant_id_column]) > 10), data)
 
 remove_empty_columns(data::DataFrame) = data[:, Not(map(col -> all(ismissing, col), eachcol(data)))]
 
-function prepare_PILT_data(
+function prepare_card_choosing_data(
     df::DataFrame;
     participant_id_column::Symbol = :participant_id,
-    filter_func::Function = (x -> !ismissing(x.trialphase) && x.trialphase == "pilt"),
+    task_name::String = "pilt",
+    filter_func::Function = (x -> !ismissing(x.trialphase) && x.trialphase == task_name),
     )
 
 	# Select rows
-	PILT_data = filter(filter_func, df)
+	task_data = filter(filter_func, df)
 
 	# Select columns
-	PILT_data = remove_empty_columns(PILT_data)
+	task_data = remove_empty_columns(task_data)
 
 	# Filter practice
-	filter!(x -> typeof(x.block) == Int64, PILT_data)
+	filter!(x -> typeof(x.block) == Int64, task_data)
 
 	# Sort
-	sort!(PILT_data, [participant_id_column, :session, :block, :trial])
+	sort!(task_data, [participant_id_column, :session, :block, :trial])
 
-	return PILT_data
+	return task_data
 
 end
 
+function prepare_reversal_data(
+    df::DataFrame;
+    participant_id_column::Symbol = :participant_id
+    )
+	reversal_data = filter(x -> x.trial_type == "reversal", df)
 
-const TASK_PREPROC_FUNCS = Dict(
-    "PILT" => prepare_PILT_data,
-    # "post_PILT_test" => prepare_test_data,
+	# Select columns
+	reversal_data = remove_empty_columns(reversal_data)
+
+	# Sort
+	sort!(reversal_data, [participant_id_column, :session, :block, :trial])
+
+	return reversal_data
+end
+
+function prepare_delay_disounting_data(
+    df::DataFrame;
+    participant_id_column::Symbol = :participant_id
+    )
+    delay_discounting_data = filter(x -> !ismissing(x.trialphase) && x.trialphase == "dd_task", df)
+
+    # Select columns
+    delay_discounting_data = remove_empty_columns(delay_discounting_data)
+
+    # Sort
+    sort!(delay_discounting_data, [participant_id_column, :session, :trial_index])
+
+    return delay_discounting_data
+end
+
+function prepare_max_press_data(
+    df::DataFrame;
+    participant_id_column::Symbol = :participant_id
+    )
+	# Define required columns for max press data
+	required_columns = [participant_id_column, :record_id, :version, :exp_start_time, :session, :trialphase, :trial_number, :avgSpeed, :responseTime, :trialPresses]
+
+	# Check and add missing columns
+	for col in required_columns
+        if !(string(col) in names(df))
+            insertcols!(df, col => missing)
+        end
+    end
+
+	# Prepare vigour data
+	max_press_data = df |>
+		x -> filter(x -> !ismissing(x.trialphase) && x.trialphase == "max_press_rate", x) |>
+		x -> select(x, 
+			participant_id_column,
+			:record_id,
+			:version,
+			:exp_start_time,
+			:session,
+			:trialphase,
+			:trial_number,
+			:avgSpeed => :avg_speed,
+			:responseTime,
+			:trialPresses => :trial_presses
+		) |>
+		x -> subset(x, 
+				[:trialphase, :trial_number] => ByRow((x, y) -> (!ismissing(x) && x in ["max_press_rate"]) || (!ismissing(y)))
+		) |>
+		x -> DataFrames.transform(x,
+			:responseTime => ByRow(JSON.parse) => :response_times
+		) |>
+		x -> select(x, 
+			Not([:responseTime])
+		)
+		# max_press_data = exclude_double_takers(max_press_data)
+	return max_press_data
+end
+
+
+TASK_PREPROC_FUNCS = Dict(
+    "PILT" => (x; kwargs...) -> prepare_card_choosing_data(x; task_name = "pilt", kwargs...),
+    "PILT_test" => (x; kwargs...) -> prepare_card_choosing_data(x; task_name = "pilt_test", kwargs...),
+    "WM" => (x; kwargs...) -> prepare_card_choosing_data(x; task_name = "wm", kwargs...),
+    "WM_test" => (x; kwargs...) -> prepare_card_choosing_data(x; task_name = "wm_test", kwargs...),
+    "reversal" => prepare_reversal_data,
+    "delay_discounting" => prepare_delay_disounting_data,
     # "vigour" => prepare_vigour_data,
     # "PIT" => prepare_PIT_data,
-    # "max_press" => prepare_max_press_data,
+    "max_press" => prepare_max_press_data,
     # "control" => prepare_control_data
 )
 
