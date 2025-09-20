068c199e164c01f873ae9d88e4082d92c0e990ab
yanivabir <yanivabir@gmail.com>
2025-09-20 21:30:06 +0000

    style: docs

--- projects/behaviour-dashboard/task-scripts/generate-figures-delay-discounting.jl
+++ projects/behaviour-dashboard/task-scripts/generate-figures-delay-discounting.jl
@@ -1,3 +1,7 @@
+# Script for estimating discount faction using logistic regression method described in 
+# Wileyto, E. P., Audrain-McGovern, J., Epstein, L. H., & Lerman, C. (2004). Using logistic regression to estimate delay-discounting functions. Behavior Research Methods, Instruments, & Computers, 36(1), 41-51.
+# https://link.springer.com/content/pdf/10.3758/BF03195548.pdf
+
 using DataFrames, CairoMakie, AlgebraOfGraphics
 
 function preprocess_delay_discounting_data(
