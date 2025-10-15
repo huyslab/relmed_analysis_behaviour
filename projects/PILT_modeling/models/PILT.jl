e00f4fd5e60bed92bf11a4d1bee9c080423845a8
yanivabir <yanivabir@gmail.com>
2025-10-15 14:12:44 +0000

    feat: work on parallaizable model

--- projects/PILT_modeling/models/PILT.jl
+++ projects/PILT_modeling/models/PILT.jl
@@ -52,3 +52,54 @@ using Distributions: Categorical
     end
 
 end
+
+@model function hierarchical_running_average2(;
+    N_actions::Int = 2, # Number of actions
+	outcomes::Vector{Matrix{Float64}}, # Outcomes for options
+    choice::Union{Vector{Vector{Missing}}, Vector{Vector{Int}}},
+    N_participants::Int,
+    participant_per_block::Vector{Int},
+    initial_value::Float64 = 0.0, # Initial Q values,
+    priors::Dict = Dict(
+            :logρ => Normal(0, 1.5),  
+            :τ => truncated(Normal(0, 0.25), 0, 100),
+    )
+)
+    
+    # Group-level hyperpriors
+    logρ ~ priors[:logρ]  # Group mean
+    τ ~ priors[:τ]  # Between-participant variability
+
+    # Participant-level random coefficients 
+    θ ~ filldist(Normal(0, 1), N_participants)
+
+    ρs = exp.(clamp.(logρ .+ τ .* θ, -5, 5))  # Individual reward sensitivites
+
+    # Run over blocks
+    for b in eachindex(outcomes)
+        Q = fill(initial_value * ρs[participant_per_block[b]], N_actions)  # Initialize
+
+        # Run over trials in block
+        for t in eachindex(choice[b])
+
+            # Learning rate
+            α = 1 / t
+
+            # Likelihood
+            choice[b][t] ~ Categorical(softmax(Q))
+
+            # Prediction error
+            PE = outcomes[b][t, choice[b][t]] * ρs[participant_per_block[b]] - Q[choice[b][t]]
+
+            # println("Block: $b, Trial: $t, Choice: $(choice[b][t]), outcome: $(outcomes[b][t, choice[b][t]]), reward: $(outcomes[b][t, choice[b][t]] * ρs[participant_per_block[b]]), PE: $PE Q: $Q, ρ: $(ρs[participant_per_block[b]]), p: $(softmax(Q))")
+
+
+            # Update Q values
+            Q[choice[b][t]] += α * PE
+
+            Q .= clamp.(Q, -50, 50)
+        end
+
+    end
+end
+
--- projects/PILT_modeling/notebooks/PILT_recovery.jl
+++ projects/PILT_modeling/notebooks/PILT_recovery.jl
@@ -101,3 +101,90 @@ fs_running_average = let running_average_ground_truth_priors = Dict(
     f1, f2
 
 end
+
+fs_running_average2 = let running_average_ground_truth_priors = Dict(
+        :logρ => Dirac(1.3),
+        :τ => Dirac(0.5)
+    ), N_participants = 10, rng = Xoshiro(0),
+    model_name = "running_average2"
+
+    n_trials = nrow(task_sequence)
+
+    task_sequences = crossjoin(
+        DataFrame(participant = 1:N_participants),
+        task_sequence
+    )
+
+    # Extract common data structures
+    outcomes = [hcat(gdf.feedback_left, gdf.feedback_right) for gdf in groupby(task_sequences, [:participant, :block])]
+    participant_per_block = unique(task_sequences[!, [:participant, :block]]).participant
+
+    missing_data_model = hierarchical_running_average2(;
+        N_actions = 2,
+        outcomes = outcomes,
+        choice = [fill(missing, nrow(gdf)) for gdf in groupby(task_sequences, [:participant, :block])],
+        N_participants = N_participants,
+        participant_per_block = participant_per_block,
+        initial_value = 0.0, # Initial Q values,
+        priors = running_average_ground_truth_priors
+    )
+
+    prior_draw = sample(
+        rng,
+        missing_data_model,
+        Prior(),
+        1
+    )
+
+    # Extract simulated data and true random effects
+    choice = extract_array_parameter(prior_draw, "choice")
+
+    function extract_first_index(param_string::String)
+        # Match the first index in brackets
+        m = match(r"\[(\d+)\]", param_string)
+        return m !== nothing ? parse(Int, m.captures[1]) : nothing
+    end
+
+    transform!(choice, :parameter => ByRow(extract_first_index) => :cblock)
+
+
+    θ = extract_array_parameter(prior_draw, "θ")
+
+
+    # Fit model to simulated data
+    data_model = hierarchical_running_average2(
+        N_actions = 2,
+        outcomes = outcomes,
+        choice = [Int.(gdf.value) for gdf in groupby(choice, :cblock)],
+        N_participants = N_participants,
+        participant_per_block = participant_per_block,
+        initial_value = 0.0, # Initial Q values,
+        priors = running_average_ground_truth_priors
+    )
+
+    # Run MCMC sampling
+    init_values = [Dict(:logρ => 1.0, :τ => 0.1, :θ => zeros(N_participants))]
+    chain = sample(rng, data_model, NUTS(), 1000; init_params = init_values)
+    # chain = load_or_run(joinpath(saved_models_dir, model_name), () -> sample(rng, data_model, NUTS(), MCMCThreads(), 1000, 4))
+
+    @info "Model fit in: $(MCMCChains.wall_duration(chain)) seconds"
+
+    # Plot fixed effects recovery
+    f1 = Figure()
+    plot_fixed_effects_recovery!(
+        f1,
+        chain,
+        running_average_ground_truth_priors
+    )
+
+    # Plot random effects recovery
+    f2 = Figure()
+    plot_random_effects_recovery!(
+        f2,
+        chain,
+        θ
+    )
+    
+    f1, f2
+
+end
